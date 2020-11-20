aaf1db84864dc94686f1b6aa2ff6c0267a915c8f
Jim Andreas <jim.r.andreas@gmail.com>
2020-11-20 12:32:07 +0100

    update versions

--- app/src/main/java/com/example/searchviewbottomnav/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/searchviewbottomnav/ui/search/SearchFragment.kt
@@ -1,3 +1,16 @@
+/*
+ *  Copyright 2020 James Andreas
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
 @file:Suppress("RedundantSamConstructor", "UNUSED_ANONYMOUS_PARAMETER")
 
 package com.example.searchviewbottomnav.ui.search
@@ -84,19 +97,6 @@ class SearchFragment :
         }
         return true
     }
-
-
-            /* override fun onQueryTextSubmit(queryText: String): Boolean {
-                 Timber.v("Submit text: $queryText")
-                 return true
-             }
-
-             override fun onQueryTextChange(queryText: String): Boolean {
- //            searchView.setCloseButtonVisibility(queryText)
-                 val searchString = queryText.trim { it <= ' ' }
-                 startSearch(searchString, false)
-                 return true
-             }*/
 
     override fun onStart() {
         super.onStart()
